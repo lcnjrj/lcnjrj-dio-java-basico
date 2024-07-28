f045a9351d92da274e882c3724b083da41bfe721
Luciana lcnjrj <lcnjrj@gmail.com>
2024-07-28 19:28:20 -0300

    v02 - tentando consertar dependencias

---
+++ Desafio-API-RESTnaNuvemSpringBoot/todo-list-v2/demo/src/main/java/com/example/demo/ToDo.java
@@ -0,0 +1,40 @@
+package com.example.todolist.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String task;
+    private boolean completed;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
